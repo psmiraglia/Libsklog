df459e8cfb3a720ec007bcfa053f3655c1d136c4
Paolo Smiraglia <paolo.smiraglia@polito.it>
2011-07-11 16:07:34 +0200

    Add license preamble to each source file

--- lib/sklog_err.h
+++ lib/sklog_err.h
@@ -1,17 +1,32 @@
+/*
+**    Copyright (C) 2011 Politecnico di Torino, Italy
+**
+**        TORSEC group -- http://security.polito.it
+**        Author: Paolo Smiraglia <paolo.smiraglia@polito.it>
+**
+**    This file is part of Libsklog.
+**
+**    Libsklog is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation; either version 2 of the License, or
+**    (at your option) any later version.
+**
+**    Libsklog is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with Libsklog.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef SKLOG_ERR_H
 #define SKLOG_ERR_H
 
-/*--------------------------------------------------------------------*/
-----------------------------------------------------------------------*/
-#define     SKLOG_ERR_INTERNAL
-#define     SKLOG_ERR_VERIFICATION
-----------------------------------------------------------------------*/
-----------------------------------------------------------------------*/
-
-----------------------------------------------------------------------*/
-----------------------------------------------------------------------*/
-
-
-int SKLOG_Error(void);
+/*
+**
+** TO IMPLEMENT
+**
+*/ 
 
 #endif /* SKLOG_ERR_H */
