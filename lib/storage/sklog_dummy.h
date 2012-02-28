792e0f5820a81f9d5cda8d24061a73ec83ccdef7
Paolo Smiraglia <paolo.smiraglia@polito.it>
2012-02-28 17:39:24 +0100

    * STORAGE:   - add dummy driver to disable storage

---
+++ lib/storage/sklog_dummy.h
@@ -0,0 +1,65 @@
+/*
+**    Copyright (C) 2011 Politecnico di Torino, Italy
+**
+**        TORSEC group -- http://security.polito.it
+**        Author: Paolo Smiraglia <paolo.smiraglia@polito.it>
+**
+**    This file is part of Libsklog.
+**
+**    Libsklog is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation; either version 2 of the License, or
+**    (at your option) any later version.
+**
+**    Libsklog is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with Libsklog.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SKLOG_DUMMY
+#define SKLOG_DUMMY
+
+#include "../sklog_commons.h"
+#include "../sklog_internal.h"
+
+#include <sys/time.h>
+#include <uuid/uuid.h>
+
+SKLOG_RETURN
+sklog_dummy_u_store_logentry(uuid_t             logfile_id,
+							 SKLOG_DATA_TYPE    type,
+							 unsigned char      *data,
+							 unsigned int       data_len,
+							 unsigned char      *hash,
+							 unsigned char      *hmac);
+
+SKLOG_RETURN
+sklog_dummy_u_flush_logfile(uuid_t    logfile_id,
+                            struct timeval *now,
+                            SKLOG_CONNECTION       *c);
+                            
+SKLOG_RETURN
+sklog_dummy_u_init_logfile(uuid_t            logfile_id,
+                           struct timeval    *t);
+                           
+SKLOG_RETURN
+sklog_dummy_t_store_authkey(char             *u_ip,
+                            uuid_t           logfile_id,
+                            unsigned char    *authkey);
+
+SKLOG_RETURN
+sklog_dummy_t_store_logentry(unsigned char    *blob,
+                             unsigned int     blob_len);
+
+SKLOG_RETURN
+sklog_dummy_t_retrieve_logfiles(unsigned char    **uuid_list,
+                                unsigned int     *uuid_list_len);
+
+SKLOG_RETURN
+sklog_dummy_t_verify_logfile(unsigned char *uuid);
+
+#endif /* SKLOG_DUMMY */
