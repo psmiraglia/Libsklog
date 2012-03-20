49fded7599325d8f27792835e02296d688630e09
Paolo Smiraglia <paolo.smiraglia@polito.it>
2012-03-20 13:40:57 +0100

    JENKINS INTEGRATION:     - revised cunit2junit.sh script

--- tests/CUnit/cunit2junit.sh
+++ tests/CUnit/cunit2junit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 for f in `ls -al *-Results.xml | cut -d" " -f9`; do
-    xsltproc --stringparam suitename Libsklog -o cunit2junit/$f cunit-to-junit.xsl $f
+    if [ ! -f cunit2junit/$f ]; then
+        xsltproc --stringparam suitename Libsklog -o cunit2junit/$f cunit-to-junit.xsl $f
+        echo OK!
+    fi
 done
